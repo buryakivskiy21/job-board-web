3926d7e12df925e0882f0b334e0765284ff1d17f
Serhiy <dev11@globalpartnersoft.com>
2023-03-27 22:17:44 +0300

    fix(urls): added func call

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,11 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("user/signUp", views.signUp, name='signUp'),
+    path("user/signUp", views.signUp.as_view(), name='signUp'),
     path("user/signIn", views.signIn.as_view(), name='signIn'),
     path("user/update", views.userUpdate.as_view(), name='userUpdate'),
 
-    path("vacancy/create", views.createVacancy, name='createVacancy'),
+    path("vacancy/create", views.createVacancy.as_view(), name='createVacancy'),
     path("vacancies/", views.getAllVacancies.as_view(), name='getAllVacancies'),
     path("vacancy/update/{id}", views.vacancyUpdate.as_view(), name='vacancyUpdate'),
     path("vacancy/delete/{id}", views.vacancyDelete.as_view(), name='vacancyDelete'),
